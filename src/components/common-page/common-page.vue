0738e3edf3af73ea70d6f37fc16b69528e43a10c
DESKTOP-RHA4O5E\ASUS <shaobing>
2024-08-08 10:40:36 +0800

    feat:通用页面

---
+++ src/components/common-page/common-page.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="common-page">
+		<!-- ios禁止弹性滚动 -->
+		<scroll-view
+			v-if="disabledIosElasticity"
+			scroll-y
+			enhanced
+			:bounces="false"
+			class="content"
+		>
+			<slot></slot>
+		</scroll-view>
+		<!-- 以前逻辑，采用默认配置 -->
+		<view v-else class="common-content">
+			<slot></slot>
+		</view>
+	</view>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+	/**
+	 * 是否禁用ios 弹性布局
+	 */
+	disabledIosElasticity: {
+		type: Boolean,
+		default: false
+	}
+});
+</script>
+
+<style lang="scss" scoped>
+.common-page {
+	display: flex;
+	flex-direction: column;
+	height: 100vh;
+	.scroll-content {
+		position: relative;
+		flex: 1;
+	}
+	.view-content {
+		overflow: auto;
+		position: relative;
+		flex: 1;
+	}
+}
+</style>
